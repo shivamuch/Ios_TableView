a236264d8e43d5028da8e74486537536f7cdf11e
Shivam Bajaj <shivam.bajaj1609@gmail.com>
2022-12-14 02:48:26 -0500

    Added project to the git repo

---
+++ ios_tableview/ios_tableviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_tableviewApp.swift
+//  ios_tableview
+//
+//  Created by Shivam Bajaj on 2022-12-14.
+//
+
+import SwiftUI
+
+@main
+struct ios_tableviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
