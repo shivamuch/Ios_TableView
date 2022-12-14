acb454c33610fc2f911d5047070eca3a0c015948
Shivam Bajaj <shivam.bajaj1609@gmail.com>
2022-12-14 03:09:33 -0500

    Added movies.swift file

---
+++ ios_tableview/Movies.swift
@@ -0,0 +1,18 @@
+//
+//  Movies.swift
+//  tableView
+//
+//  Created by Shivam
+//
+
+import Foundation
+
+// data model
+class Movies: NSObject{
+    
+    var MovieName: String
+    init(MovieName: String){
+        self.MovieName = MovieName
+    }
+    
+}
